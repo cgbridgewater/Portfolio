748c6a16e40abcf75a8ee2772449c290b318bd19
Chris Bridgewater <cgbridgewater@outlook.com>
2023-07-13 01:45:21 -0700

    add images and socials

--- client/src/Components/IndexPage.jsx
+++ client/src/Components/IndexPage.jsx
@@ -1,129 +1,121 @@
 const getOutside = require(`../Assets/Images/getOutside.png`);
 const candyShop = require(`../Assets/Images/candyShop.png`);
 const petShelter = require(`../Assets/Images/petShelter.png`);
+const tetris = require(`../Assets/Images/MyTetris.png`);
+const drawIt = require(`../Assets/Images/DrawIt.png`);
 
 const Index = () => {
 
   return (
 
     <div>
-            
-      {/* Nav Bar*/}
+      {/* NAV BAR*/}
       <nav id="navbar">
-
         <ul>
-
-          <li><a class="section-link" href="#welcome-section"><i class="fas fa-chevron-right"></i>Welcome</a></li>
-
-          <li><a class="section-link" href="#projects"><i class="fas fa-chevron-right"></i>Projects</a></li>
-
-          <li><a class="section-link" href="#contact-section"><i class="fas fa-chevron-right"></i>Contact</a></li>
-
+          <li><a className="section-link" href="#welcome-section"><i className="fas fa-chevron-right"></i>Welcome</a></li>
+          <li><a className="section-link" href="#projects"><i className="fas fa-chevron-right"></i>Projects</a></li>
+          <li><a className="section-link" href="#contact-section"><i className="fas fa-chevron-right"></i>Contact</a></li>
         </ul>
-
       </nav>
-
-      {/* Welcome / Name */}
+      {/* NAV BAR END*/}
+      {/* WELCOME */}
       <section id="welcome-section">
-
         <h1>Chris Bridgewater <br />
-        Full Stack Web Developer</h1>
-
+        Full Stack Web Developer
+        <br /><span className="titleSpan">MERN / Java / Python</span>
+        </h1>
         <a id="link-to-contact" href="#contact-section">Contact me</a>
-
-        <a id="link-to-projects" href="#projects"><i class="fa-solid fa-angle-down fa-bounce fa-2xl" style={{color:"#eb6028"}}></i></a>
-
+        <a id="link-to-projects" href="#projects"><i className="fa-solid fa-angle-down fa-bounce fa-2xl" style={{color:"#eb6028"}}></i></a>
       </section>
-
+      {/* WELCOME END */}
       {/* PROJECTS */}
       <section id="projects">
-
         <h2>My projects:</h2>
-
         <div id="projects-gallery">
-
-          <div class="project-tile">
+          {/* project 1 */}
+          <div className="project-tile">
             <a href="http://52.33.105.171/" target="_blank" rel="noreferrer noopener">
-              <img class="webpage-img" src={candyShop} alt="webpage screen"/>
+              <img className="webpage-img" src={candyShop} alt="webpage screen"/>
             </a>
-            <p class="project-caption">Candy Shop</p>
+            <p className="project-caption">Candy Shop</p>
           </div>
-
-          <div class="project-tile">
+          {/* project 2 */}
+          <div className="project-tile">
             <a href="http://54.201.23.108/getoutside" target="_blank" rel="noreferrer noopener">
-              <img class="webpage-img" src={getOutside} alt="webpage screen"/>
+              <img className="webpage-img" src={getOutside} alt="webpage screen"/>
             </a>
-            <p class="project-caption">Get Outside</p>
+            <p className="project-caption">Get Outside</p>
           </div>
-
-          <div class="project-tile">
+          {/* project 3 */}
+          <div className="project-tile">
             <a href="http://34.218.236.175/" target="_blank" rel="noreferrer noopener">
-              <img class="webpage-img" src={petShelter} alt="webpage screen"/>
+              <img className="webpage-img" src={petShelter} alt="webpage screen"/>  
             </a>
-            <p class="project-caption">Pet Shelter</p>
+            <p className="project-caption">Pet Shelter</p>
           </div>
-
-          <div class="project-tile">
+          {/* project 4 */}
+          <div className="project-tile">
+            <a href="http://35.88.143.101/" target="_blank" rel="noreferrer noopener">
+              <img className="webpage-img" src={tetris} alt="webpage screen"/>
+            </a>
+            <p className="project-caption">Tetris PC Mini Game</p>
+          </div>
+          {/* project 5 */}
+          <div className="project-tile">
+            <a href="#" target="_blank" rel="noreferrer noopener">
+              <img className="webpage-img" src={drawIt} alt="webpage screen"/>
+            </a>
+            <p className="project-caption">Draw It PC Mini Game</p>
+          </div>
+          {/* project 6 */}
+          <div className="project-tile">
             <div>
-              <div class="placeholder-img">
+              <div className="placeholder-img">
                 <p>Coming soon</p>
               </div>
             </div>
-            <p class="project-caption">Other</p>
+            <p className="project-caption">Other</p>
           </div>
-
-          <div class="project-tile">
-            <div>
-              <div class="placeholder-img">
-                <p>Coming soon</p>
-              </div>
-            </div>
-            <p class="project-caption">Other</p>
-          </div>
-
-          <div class="project-tile">
-            <div>
-              <div class="placeholder-img">
-                <p>Coming soon</p>
-              </div>
-            </div>
-            <p class="project-caption">Other</p>
-          </div>
-
         </div>
-
-        <a id="link-to-contact" href="#contact-section">Contact me</a>
-
+        {/* CONTACT LINK */}
+        <br /><a id="link-to-projects" href="#contact"><i className="fa-solid fa-angle-down fa-bounce fa-2xl" style={{color:"#eb6028"}}></i></a>
+        {/* CONTACT LINK END */}
       </section>
-
+      {/* PROJECT END */}
       {/* CONTACT */}
       <section id="contact-section">
-
         <div id="contact">
-
+          {/* CONTACT TITLE */}
           <h2>Contact Me</h2>
-
           <h3>You can find me on my social media or send email:</h3>
-
+          {/* CONTACT TITLE END */}
+          {/* MEDIA LINKS */}
           <ul id="links-list">
-            <li><a href="https://www.linkedin.com/in/chris-bridgewater/" target="_blank" rel="noreferrer noopener"><i class="fa-brands fa-linkedin"></i>Linked In</a></li>
-            <li><a href="https://github.com/cgbridgewater" target="_blank" rel="noreferrer noopener"><i class="fab fa-github-square"></i>GitHub</a></li>
-            <li><a href="https://www.instagram.com/cgbridgewater/" target="_blank" rel="noreferrer noopener"><i class="fab fa-instagram-square"></i>Instagram</a></li>
-            <li><a href="mailto:cgbridgewater@outlook.com?subject=Web Development Contact"><i class="fas fa-envelope"></i>Email</a></li>
+            <li><a href="https://www.linkedin.com/in/chris-bridgewater/" target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-linkedin"></i>Linked In</a></li>
+            <li><a href="https://github.com/cgbridgewater" target="_blank" rel="noreferrer noopener"><i className="fab fa-github-square"></i>GitHub</a></li>
+            <li><a href="https://www.instagram.com/cgbridgewater/" target="_blank" rel="noreferrer noopener"><i className="fab fa-instagram-square"></i>Instagram</a></li>
+            <li><a href="mailto:cgbridgewater@outlook.com?subject=Web Development Contact"><i className="fas fa-envelope"></i>Email</a></li>
+            <li><a href="#"><i className="fas fa-file-alt" style={{color:"#EB6028"}}></i>Resume</a></li>
           </ul>
-
+          {/* MEDIA LINKS END */}
+          {/* PROFILE IMAGE */}
+          <div className="card card0">
+            <div className="border">
+              <h2 className="cardName">Chris Bridgewater</h2>
+              <h3 className="cardName">Web Developer</h3>
+            </div>
+          </div>
+          {/* PROFILE IMAGE END */}
         </div>
-
         {/* FOOTER */}
         <div id="footer-container">
           <footer>
-            <p>Photo by <a href="https://unsplash.com/@florianolv?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Florian Olivo</a> on <a href="https://unsplash.com/s/photos/code?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></p>
-
-            <p>Created 2023</p>
+            <p>Created 07/2023</p>
           </footer>
         </div>
+        {/* FOOTER END */}
       </section>
-
+      {/* CONTACT END */}
     </div>
   );
 };
