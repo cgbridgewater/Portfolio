d5adc1731084b3135dc783764556b89b37ab079f
cgbridgewater <cgbridgewater@outlook.com>
2024-01-16 22:26:02 -0800

    add font

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fontsource/syncopate"; 
 import Origin from './Views/Origin';
 import Index from './Views/Index';
 
