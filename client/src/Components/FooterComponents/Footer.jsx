1c47d05b104d7b4ac07949e6da351b4e648d54c9
cgbridgewater <cgbridgewater@outlook.com>
2024-03-10 23:47:18 -0700

    update footer

--- client/src/Components/FooterComponents/Footer.jsx
+++ client/src/Components/FooterComponents/Footer.jsx
@@ -5,7 +5,7 @@ function Footer (){
     return(
     <div id="footer-container">
         <footer>
-            <p>Rev. 1/2024 - Happy New Years!<i className="fas fa-glass-cheers" style={{color:"#eb6028"}}></i></p>
+            <p>Rev. 4/2024</p>
         </footer>
     </div>
     )
